0e49328b1fb40a16337dc405ecd44d8c3c25af8f
dorukwagle <dorukwagle@gmail.com>
2024-06-27 09:49:20 +0545

    support directory tree

--- ImageCompressor.py
+++ ImageCompressor.py
@@ -1,14 +1,10 @@
 from PIL import Image
+from threading import Thread
+import subprocess as sp
 import os
 import sys
 import time
-from threading import Thread
-
-
-def get_parent_dir(path):
-    pth = path.split('/')
-    pth.pop()
-    return '/'.join(pth)
+from pathlib import Path
 
 
 def get_filename(path):
@@ -16,19 +12,27 @@ def get_filename(path):
     return filename  # , *(filename.split('.'))
 
 
-def readjust_image(img_path, quality=50, dpi=(72, 72), format='', size='', verbose=False):
+def list_files(folder_path):
+    run = sp.run(['find', folder_path], capture_output=True)
+    return run.stdout.decode('utf-8').strip().split('\n')
+
+
+def get_destination_file(base_path, file_path):
+    new_dest = f'{base_path}/adjusted'
+    return f"{new_dest}{file_path.replace(base_path, '')}"
+
+
+def readjust_image(base_path, img_path, quality=50, dpi=(72, 72), format='', size='', verbose=False):
     with Image.open(img_path) as img:
         img_size = size or img.size
         img_format = format or img.format
 
-        parent_dir = get_parent_dir(img_path)
-        filename = get_filename(img_path)
-        new_path = parent_dir + '/adjusted'
+        destination_file = get_destination_file(base_path, img_path)
 
-        if not os.path.exists(new_path):
-            os.mkdir(new_path)
-
-        destination_file = f'{new_path}/{filename}'
+        # create destination folder if not exist
+        path = Path(destination_file)
+        path = path.parent
+        path.mkdir(parents=True, exist_ok=True)
 
         img = img.resize(img_size, Image.Resampling.LANCZOS)
         img.save(destination_file, img_format, optimize=True, quality=quality, dpi=dpi, lossless=False)
@@ -37,7 +41,7 @@ def readjust_image(img_path, quality=50, dpi=(72, 72), format='', size='', verbo
             print(img_path, ' >> ', destination_file)
 
 
-def help():
+def help_text():
     return '''
         ImageCompressor <source_folder> <verbose>(opt) <quality>(opt)
         e.g.: ImageCompressor /home/doruk/Pictures -v 80 
@@ -47,7 +51,7 @@ def help():
 def execute_in_threads(folder_path, quality, verbose):
     total_threads = 8
 
-    files = os.listdir(folder_path)
+    files = list_files(folder_path)
 
     total_threads = min(len(files), total_threads)
     chunk_size = len(files) // total_threads
@@ -55,10 +59,10 @@ def execute_in_threads(folder_path, quality, verbose):
 
     def perform_task(start_index, end_index):
         for fl_index in range(start_index, end_index):
-            fl = f'{folder_path}/{files[fl_index]}'
+            fl = files[fl_index]
 
             if os.path.isfile(fl):
-                readjust_image(fl, quality=int(quality), verbose=verbose, dpi=(300, 300))
+                readjust_image(folder_path, fl, quality=int(quality), verbose=verbose, dpi=(300, 300))
 
     for i in range(0, total_threads):
         start_pos = i * chunk_size
@@ -78,26 +82,30 @@ def execute_in_threads(folder_path, quality, verbose):
 
 
 def execute(folder_path, quality, verbose):
-    for fl in os.listdir(folder_path):
-        fl = f'{folder_path}/{fl}'
-
+    for fl in list_files(folder_path):
         if os.path.isfile(fl):
-            readjust_image(fl, quality=int(quality), verbose=verbose, dpi=(300, 300))
+            readjust_image(folder_path, fl, quality=int(quality), verbose=verbose, dpi=(300, 300))
 
 
 def main():
     args = sys.argv
     if len(args) < 2:
-        print(help())
+        print(help_text())
         exit('no source directory given')
 
-    folder_path = args[1]
+    folder_path = args[1][0:-1] if args[1][-1] == '/' else args[1]
+
     quality = args[2] if len(args) > 2 else 80
     verbose = '-v' in args
     parallel = '--threads' in args
 
     if (not os.path.exists(folder_path)) or (not os.path.isdir(folder_path)):
         exit('given path must be a directory')
+
+    # create destination folder if not exist
+    dest_path = f"{folder_path}/adjusted"
+    if not os.path.exists(dest_path):
+        os.mkdir(dest_path)
 
     start = time.time()
     if parallel:
